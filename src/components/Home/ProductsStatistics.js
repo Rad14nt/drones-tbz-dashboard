819387f181db2233dbc3bfca9062d94611a9530a
Rad14nt <andrei@gimli.ro>
2023-01-30 23:47:38 +0100

    chart update

--- src/components/Home/ProductsStatistics.js
+++ src/components/Home/ProductsStatistics.js
@@ -15,7 +15,7 @@ const ProductsStatistics = () => {
               width: "100%",
               height: "350px",
             }}
-            src="https://charts.mongodb.com/charts-shoeshoptutorial-bzbxw/embed/charts?id=1f926980-090b-44c6-b011-3e94b2efddca&maxDataAge=3600&theme=light&autoRefresh=true"
+            src="https://charts.mongodb.com/charts-drones-lcymw/embed/charts?id=63d8486e-9323-49ad-86a5-1f2c52a80e5d&maxDataAge=3600&theme=light&autoRefresh=true"
           ></iframe>
         </article>
       </div>
